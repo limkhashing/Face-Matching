3e84d84c143876a2b2a2bdc831b08e37b79502e7
Lim Kha Shing <kslim5703@gmail.com>
2019-12-07 01:17:31 +0800

    update pytesseract path

--- src/OCR/process_ocr.py
+++ src/OCR/process_ocr.py
@@ -6,7 +6,8 @@ from src.OCR.regex_patterns import IC_PATTERNS, DRIVING_PATTERN, IC_NUMBER_REGRE
     DRIVING_DATE_REGREX, DRIVING_IC_NUMBER_REGREX, PASSPORT_DATE_REGREX, PASSPORT_PATTERNS
 
 # If you don't have tesseract executable in your PATH, include the following:
-pytesseract.pytesseract.tesseract_cmd = r'D:/Tesseract-OCR/tesseract.exe'
+# pytesseract.pytesseract.tesseract_cmd = r'D:/Tesseract-OCR/tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 
 IDENTITY_CARD = "IDENTITY CARD"
 DRIVING_LICENSE = "DRIVING LICENSE"
